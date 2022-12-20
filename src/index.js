229fac75732bbefb1fac50e0b5199cc42cec45a2
gijoe2day <gjuliao32@hotmail.com>
2022-12-20 18:23:02 -0500

    changed position of fetch data

--- src/index.js
+++ src/index.js
@@ -5,53 +5,50 @@ const form = document.querySelector('#add_score');
 const refresh = document.querySelector('#btnRefresh');
 
 const asyncPost = async () => {
-    const pName = document.getElementById('player_name').value;
-    const pScore = document.getElementById('player_score').value;
-    console.log(pName, pScore);
-    try {
-        const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/53JtqdHuzGVKECuyF7mz/scores/', {
-            method: 'POST',
-            body: JSON.stringify({
-                user: pName,
-                score: pScore,
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        });
-        const data = await response.json();
-        console.log(data);
-    }
-    catch (error) {
-        console.log(error);
-    }
+  const pName = document.getElementById('player_name').value;
+  const pScore = document.getElementById('player_score').value;
+  console.log(pName, pScore);
+  try {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/53JtqdHuzGVKECuyF7mz/scores/', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: pName,
+        score: pScore,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-
 const fetchData = async () => {
-    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/53JtqdHuzGVKECuyF7mz/scores/')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data.result);
-    let output = '';
-    data.result.forEach((el) => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/53JtqdHuzGVKECuyF7mz/scores/')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.result);
+      let output = '';
+      data.result.forEach((el) => {
         output += `
         <li>name: ${el.user}, score: ${el.score}</li>
         `;
+      });
+      document.querySelector('#score_listing').innerHTML = output;
     });
-    document.querySelector('#score_listing').innerHTML = output;
-  });
 };
 
-
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    asyncPost();
-  })
+  e.preventDefault();
+  asyncPost();
+});
 
 refresh.addEventListener('click', () => {
-    console.log('refresh clicked');
-    document.querySelector('#score_listing').innerHTML = ''
+  console.log('refresh clicked');
+  document.querySelector('#score_listing').innerHTML = '';
 });
 
 document.addEventListener('DOMContentLoaded', fetchData());
